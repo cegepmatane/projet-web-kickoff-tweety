64c66235e559ed378e008f73fe50cc4647b9aec7
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-20 18:16:13 -0500

    Ajout fonction requete()

--- POCTwitterClone/index.php
+++ POCTwitterClone/index.php
@@ -9,7 +9,13 @@ $conn = mysqli_connect($hotebd, $utilisateurbd, $motdepassebd);
 
 mysqli_select_db($conn, $nombd);
 
-$res = mysqli_query($conn, "select * from utilisateurs");
+
+$res = requete("select * from utilisateurs");
 while ($li = mysqli_fetch_assoc($res)) {
     print_r($li);
+}
+
+function requete($requete) {
+    global $conn;
+    return mysqli_query($conn, $requete);
 }
