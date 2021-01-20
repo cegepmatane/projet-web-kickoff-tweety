4daa7151e6d63b9f5bbf27f2645239bd65206505
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-20 17:56:52 -0500

    Ajout bd et requête de test

--- POCTwitterClone/index.php
+++ POCTwitterClone/index.php
@@ -1,3 +1,15 @@
 <?php
 
-print 'Hello World';
+$hotebd = 'localhost';
+$utilisateurbd = 'root';
+$motdepassebd = '';
+$nombd = 'poctwitterclone';
+
+$conn = mysqli_connect($hotebd, $utilisateurbd, $motdepassebd);
+
+mysqli_select_db($conn, $nombd);
+
+$res = mysqli_query($conn, "select * from utilisateurs");
+while ($li = mysqli_fetch_assoc($res)) {
+    print_r($li);
+}
