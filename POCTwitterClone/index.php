eb8455bcc5a4c92d09fd9919df2c48e738fb55ad
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-20 18:53:07 -0500

    Enregistrement tweets dans bd

--- POCTwitterClone/index.php
+++ POCTwitterClone/index.php
@@ -16,11 +16,13 @@ while ($li = mysqli_fetch_assoc($res)) {
 }
 */
 
+// Execute une requête
 function requete($requete) {
     global $conn;
     return mysqli_query($conn, $requete);
 }
 
+/** Récupère la première ligne d'une requête */
 function getTete($requete) {
     $res = requete($requete);
     $li = mysqli_fetch_row($res);
@@ -28,8 +30,20 @@ function getTete($requete) {
 }
 
 if ($_REQUEST['tweet']) {
-    $tweet = $_REQUEST['tweet'];
-    $ip = $_SERVER['REMOTE_ADDR'];
+    // Récupérer le contenu du tweet de l'utilisateur
+    $tweet = mysqli_real_escape_string($conn, $_REQUEST['tweet']);
+    // Récupérer l'adresse ip de l'utilisateur
+    $ip = mysqli_real_escape_string($conn, $_SERVER['REMOTE_ADDR']);
+
+    // Si l'utilisateur qui tweete n'existe pas, le créer
+    $uid = getTete("select uid from user where ip = " . $ip);
+    if (!$uid) {
+        requete("insert into utilisateurs(ip) values ('$ip')");
+    }
+    // Enregistrer le tweet
+    $date = Date("Y-m-d H:i:s");
+    requete("insert into tweets(uid, post, date) values('$uid', '$tweet', '$date')");
+
     print "$tweet, $ip";
 }
 
