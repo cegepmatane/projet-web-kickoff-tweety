6b23293001c5d1ef33b4ba1086bbfb3a873decf1
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-20 18:18:12 -0500

    Ajout formulaire pour poster

--- POCTwitterClone/index.php
+++ POCTwitterClone/index.php
@@ -9,13 +9,23 @@ $conn = mysqli_connect($hotebd, $utilisateurbd, $motdepassebd);
 
 mysqli_select_db($conn, $nombd);
 
-
+/*
 $res = requete("select * from utilisateurs");
 while ($li = mysqli_fetch_assoc($res)) {
     print_r($li);
 }
+*/
 
 function requete($requete) {
     global $conn;
     return mysqli_query($conn, $requete);
 }
+
+print <<< EOF
+
+<form action=index.php>
+    <textarea name="tweet"></textarea>
+    <input type="submit" value="Tweet">
+</form>
+
+EOF;
