951fd2906a21629ba967c8763355b9214a41ba6c
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-22 09:05:25 -0500

    Ajout fonction getUid()

--- POCTwitterClone/donnees/TweetDAO.php
+++ POCTwitterClone/donnees/TweetDAO.php
@@ -5,6 +5,7 @@ require_once ('./modeles/Tweet.php');
 
 class TweetDAO extends DAO {
 
+    /** Retourne un array de tous les tweets */
     public function getTweets(): array {
         $res = $this->requete("select * from tweets order by date desc");
         $tweets = array();
@@ -12,5 +13,12 @@ class TweetDAO extends DAO {
             $tweets[] = new Tweet($tweet['tid'], $tweet['uid'], $tweet['post'], $tweet['date']);
         }
         return $tweets;
+    }
+
+    /** Retourne l'id de l'utilisateur connecté */
+    public function getUid() {
+        global $conn;
+        $ip = mysqli_real_escape_string($conn, $_SERVER['REMOTE_ADDR']);
+        return $this->getLigne("select uid from utilisateurs where ip='$ip'");
     }
 }
