815b0af2241ba08e68ae6d1ef2570af9d5df75cd
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-22 09:24:48 -0500

    Ajout fonctionnalité tweeter

--- POCTwitterClone/controllers/c_accueil.php
+++ POCTwitterClone/controllers/c_accueil.php
@@ -4,6 +4,15 @@ require_once ('./donnees/TweetDAO.php');
 
 $tweetDAO = new TweetDAO();
 
+if ($_REQUEST['tweet']) {
+    // Récupérer le contenu du tweet de l'utilisateur
+    $tweet = $_REQUEST['tweet'];
+    // Récupérer l'adresse ip de l'utilisateur
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    $tweetDAO->addTweet($tweet, $ip);
+}
+
 $tweets = $tweetDAO->getTweets();
 
 $tweetsSuivis = $tweetDAO->getTweetsSuivis();
