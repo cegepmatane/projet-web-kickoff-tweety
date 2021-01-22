68709f403f1b539fa89938c2c0e91fc8a4bfb2df
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-21 19:59:50 -0500

    Ajout v_header.php

---
+++ POCTwitterClone/vues/v_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device, initial-scale=1.0">
+        <title>Twitter Clone</title>
+
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+    <body class="px-3 flex flex-col h-screen justify-between">
+        <!-- En-tête -->
+        <header></header>
+        <!-- Menu -->
+        <?php include('vues/v_menu.php'); ?>
+        <!-- Vue -->
+        <section class="mb-auto">
