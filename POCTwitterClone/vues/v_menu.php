99ac313fdb191d66ea8832dc1ddf40a9beadb881
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-21 20:00:00 -0500

    Ajout v_menu.php

---
+++ POCTwitterClone/vues/v_menu.php
@@ -0,0 +1,11 @@
+<!-- Menu du site -->
+<nav class="mb-5 flex flex-row">
+    <div>
+        <ul>
+            <li>
+                <a href="../index.php" class="text-indigo-500
+                hover:text-indigo-600 underline">Accueil </a>
+            </li>
+        </ul>
+    </div>
+</nav>
