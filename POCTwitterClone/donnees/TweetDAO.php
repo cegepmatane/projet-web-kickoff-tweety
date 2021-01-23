b7bcab4f55e2a95557559d764cdcfb099f81303f
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-23 09:19:59 -0500

    Refactorisation TweetDAO.php en français

--- POCTwitterClone/donnees/TweetDAO.php
+++ POCTwitterClone/donnees/TweetDAO.php
@@ -6,12 +6,12 @@ require_once ('./modeles/Tweet.php');
 class TweetDAO extends DAO {
 
     /** Retourne un array de tous les tweets */
-    public function getTweets(): array {
+    public function obtenirTweets(): array {
         $res = $this->requete("select * from tweets order by date desc");
         $tweets = array();
         while ($tweet = mysqli_fetch_assoc($res)) {
             $suivi = false;
-            if ($this->estUnFollower($this->getUid(), $tweet['uid'])) {
+            if ($this->estUnFollower($this->obtenirUtilisateur(), $tweet['uid'])) {
                 $suivi = true;
             }
             $tweets[] = new Tweet($tweet['tid'], $tweet['uid'], $tweet['post'], $tweet['date'], $suivi);
@@ -20,16 +20,16 @@ class TweetDAO extends DAO {
     }
 
     /** Retourne un array des tweets des utilisateurs suivis */
-    public function getTweetsSuivis($utilisateur = false): array {
+    public function obtenirTweetsSuivis($utilisateur = false): array {
         if ($utilisateur === false) {
-            $utilisateur = $this->getUid();
+            $utilisateur = $this->obtenirUtilisateur();
         }
         $res = $this->requete("select * from tweets where uid in (select follower from follows where uid='$utilisateur') 
         order by date desc");
         $tweets = array();
         while ($tweet = mysqli_fetch_assoc($res)) {
             $suivi = false;
-            if ($this->estUnFollower($this->getUid(), $tweet['uid'])) {
+            if ($this->estUnFollower($this->obtenirUtilisateur(), $tweet['uid'])) {
                 $suivi = true;
             }
             $tweets[] = new Tweet($tweet['tid'], $tweet['uid'], $tweet['post'], $tweet['date'], $suivi);
@@ -38,17 +38,17 @@ class TweetDAO extends DAO {
     }
 
     /** Ajoute un tweet */
-    public function addTweet($tweet, $ip): void {
+    public function ajouterTweet($tweet, $ip): void {
         $tweet = $this->filter($tweet);
         $ip  = $this->filter($ip);
         // Si l'utilisateur qui tweete n'existe pas, le créer
-        $uid = $this->getUid();
-        if (!$uid) {
+        $utilisateur = $this->obtenirUtilisateur();
+        if (!$utilisateur) {
             requete("insert into utilisateurs(ip) values ('$ip')");
         }
         // Enregistrer le tweet
         $date = Date("Y-m-d H:i:s");
-        $this->requete("insert into tweets(uid, post, date) values('$uid', '$tweet', '$date')");
+        $this->requete("insert into tweets(uid, post, date) values('$utilisateur', '$tweet', '$date')");
     }
 
     /** Retourne true si l'utilisateur suit l'autre utilisateur */
@@ -62,7 +62,7 @@ class TweetDAO extends DAO {
     }
 
     /** Retourne l'id de l'utilisateur connecté */
-    public function getUid() {
+    public function obtenirUtilisateur() {
         $ip = $this->filter($_SERVER['REMOTE_ADDR']);
         return $this->getLigne("select uid from utilisateurs where ip='$ip'");
     }
