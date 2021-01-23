70631fa2a9a001a3d92bad087126acb3d2d1afdb
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-23 09:56:19 -0500

    Ajout ajouterAbonnement()

--- POCTwitterClone/donnees/UtilisateurDAO.php
+++ POCTwitterClone/donnees/UtilisateurDAO.php
@@ -3,4 +3,17 @@
 require_once ('DAO.php');
 
 class UtilisateurDAO extends DAO {
+
+    /** Abonne l'utilisateur à l'utilisateur spécifié */
+    public function ajouterAbonnement($follower): void {
+        $follower = $this->filter($follower);
+        $utilisateur = $this->obtenirUtilisateur();
+        $this->requete("insert into follows(uid, follower) values ('$utilisateur', '$follower')");
+    }
+
+    /** Retourne l'id de l'utilisateur connecté */
+    public function obtenirUtilisateur() {
+        $ip = $this->filter($_SERVER['REMOTE_ADDR']);
+        return $this->getLigne("select uid from utilisateurs where ip='$ip'");
+    }
 }
