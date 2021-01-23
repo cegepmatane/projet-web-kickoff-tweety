93922b1d29285647259f36cf2c73c6408af74cab
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-23 09:56:47 -0500

    Ajout retirerAbonnement()

--- POCTwitterClone/donnees/UtilisateurDAO.php
+++ POCTwitterClone/donnees/UtilisateurDAO.php
@@ -11,6 +11,13 @@ class UtilisateurDAO extends DAO {
         $this->requete("insert into follows(uid, follower) values ('$utilisateur', '$follower')");
     }
 
+    /** Désabonne l'utilisateur de l'utilisateur spécifié */
+    public function retirerAbonnement($follower): void {
+        $follower = $this->filter($follower);
+        $utilisateur = $this->obtenirUtilisateur();
+        $this->requete("delete from follows where uid='$utilisateur' and follower='$follower'");
+    }
+
     /** Retourne l'id de l'utilisateur connecté */
     public function obtenirUtilisateur() {
         $ip = $this->filter($_SERVER['REMOTE_ADDR']);
