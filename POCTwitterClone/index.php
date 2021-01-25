bfbadabd67112a3c77ab3d53d0a04ad6879eb8c7
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-25 16:14:03 -0500

    Routage index.php #1

--- POCTwitterClone/index.php
+++ POCTwitterClone/index.php
@@ -1,3 +1,9 @@
 <?php
 
-require_once('controllers/c_accueil.php');
+if (isset($_GET['page'])) {
+    $page = htmlspecialchars($_GET['page']);
+} else {
+    $page = "accueil";
+}
+
+require_once('controllers/c_'.$page.'.php');
