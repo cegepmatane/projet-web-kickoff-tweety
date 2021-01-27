610c2ea99a29ce2bf5a5485d919f94f5c09cdd65
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-27 12:33:10 -0500

    Ajout ajouterTweet() #5

--- POCTwitterClone/accesseur/TweetDAO.php
+++ POCTwitterClone/accesseur/TweetDAO.php
@@ -20,6 +20,28 @@ class Accesseur {
 class TweetDAO extends Accesseur implements TweetSQL {
 
 
+    /** Ajoute un tweet */
+    public function ajouterTweet($tweet): void {
+        self::initialiser();
+
+        // Si l'utilisateur qui tweete n'existe pas, le créer
+        $utilisateur = $this->obtenirUtilisateur();
+        if (!$utilisateur) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+            $requete = self::$bd->prepare(self::SQL_AJOUTER_UTILISATEUR);
+            $requete->bindParam(':ip', $ip, PDO::PARAM_STR);
+            $requete->execute();
+        }
+
+        // Enregistrer le tweet
+        $date = Date("Y-m-d H:i:s");
+        $requete = self::$bd->prepare(self::SQL_AJOUTER_TWEET);
+        $requete->bindParam(':uid', $utilisateur, PDO::PARAM_INT);
+        $requete->bindParam(':post', $tweet, PDO::PARAM_STR);
+        $requete->bindParam(':date', $date, PDO::PARAM_STR);
+        $requete->execute();
+    }
+
     /** Retourne true si l'utilisateur suit l'autre utilisateur sinon false*/
     public function estUnFollower($utilisateur, $follower): bool {
         self::initialiser();
