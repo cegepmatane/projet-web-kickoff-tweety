f2abd9695d7fb82683e51c3377fad41a6c1ddab9
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-02-05 12:13:38 -0500

    Footer #8

--- doc/prototypes/accueil/accueil.php
+++ doc/prototypes/accueil/accueil.php
@@ -14,7 +14,7 @@
     require_once ('Tweet.php');
 
     $tweets = array();
-    for ($i=0;$i<5;$i++) {
+    for ($i=0;$i<2;$i++) {
         for ($j=0;$j<2;$j++) {
             $tweets[] = new Tweet(array(
                 'tid' => $i+$j,
