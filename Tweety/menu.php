902c42c0eacecc1276e324551cd6cc8db6017e1f
YohannVitale <31995754+YohannVitale@users.noreply.github.com>
2021-02-10 09:49:48 -0500

    Navigation

--- Tweety/menu.php
+++ Tweety/menu.php
@@ -1,11 +1,22 @@
 <!-- Menu du site -->
-<nav>
-    <ul id="menu">
-        <li><a class="menu-item">tweety.</a></li>
-        <li><a class="menu-item" href="">Accueil</a></li>
-        <li><a class="menu-item" href="">Suivis</a></li>
-        <li><a class="menu-item" href="">Profil</a></li>
-        <li><a class="menu-item" href="">Administration</a></li>
-        <li><a class="action" href="">Déconnexion</a></li>
-    </ul>
-</nav>
+<!--<nav class="mb-5 flex flex-row">
+    <div>
+        <ul>
+            <li>
+                <a href="index.php" class="mr-2">Accueil</a>
+                <a href="administration.php" class="mr-2">Administration</a>
+            </li>
+        </ul>
+    </div>
+</nav> -->
+<!-- Menu -->
+        <nav>
+            <ul id="menu">
+                <li><a class="action">Tweety</a></li>
+                <li><a class="menu-item" href="index.php">Accueil</a></li>
+                <li><a class="menu-item" href="suivis.php">Suivis</a></li>
+                <li><a class="menu-item" href="profil.php">Profil</a></li>
+                <li><a class="menu-item" href="administration.php">Administration</a></li>
+                <li><a class="action" href="">Déconnexion</a></li>
+            </ul>
+        </nav>
