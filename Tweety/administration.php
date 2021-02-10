3c8c3b66316f5c1a809989e37d99af59a14f21f1
YohannVitale <31995754+YohannVitale@users.noreply.github.com>
2021-02-10 09:48:26 -0500

    Bug css

--- Tweety/administration.php
+++ Tweety/administration.php
@@ -12,25 +12,36 @@ $tweetDAO = new TweetDAO();
 $tweets = TweetDAO::listerTweets();
 ?>
 
-<!-- Affichage tweets -->
-<table class="mt-5 table-auto">
-    <?php foreach ($tweets as $tweet): ?>
-        <tr>
-            <td><a href=" " title="<?= UtilisateurDAO::obtenirBiographie($tweet->uid)?>" style="background-color:#FFFFFF;color:#000000;text-decoration:none;cursor:help;"><?=UtilisateurDAO::obtenirPseudonyme($tweet->uid)?></a></td>
-            <td><?=$tweet->post?></td>
-            <td><?=$tweet->date?></td>
-            <td><a href="modifier.php?tid=<?=$tweet->tid?>">Modifier</a></td>
-            <td>
-                <form action="" method="post">
-                    <input type="hidden" name="tid" value="<?=$tweet->tid?>"/>
-                    <div>
-                        <input type="submit" name="action-supprimer" value="Supprimer"/>
-                    </div>
-                </form>
-            </td>
-        </tr>
-    <?php endforeach; ?>
-</table>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="decoration/styles2.css">
+</head>
 
+<section>
+    <div id="tweets">
+        <!-- Affichage tweets -->
+        <table>
+            
+            <?php foreach ($tweets as $tweet): ?>
+                <div class="tweet">
+                    <td><a href=" " title="<?= UtilisateurDAO::obtenirBiographie($tweet->uid)?>"><?=UtilisateurDAO::obtenirPseudonyme($tweet->uid)?></a></td>
+                    <td><?=$tweet->post?></td>
+                    <td><?=$tweet->date?></td>
+                    <td><a class="action" href="modifier.php?tid=<?=$tweet->tid?>">Modifier</a></td>
+                    <td>
+                        <form action="" method="post">
+                            <input type="hidden" name="tid" value="<?=$tweet->tid?>"/>
+                            <div>
+                                <input type="submit" class="action" name="action-supprimer" value="Supprimer"/>
+                            </div>
+                        </form>
+                    </td>
+                </div>            
+            <?php endforeach; ?>
+            
+        </table>
+    </div>
+</section>
 <!--  Pied de page -->
 <?php require_once('footer.php');
