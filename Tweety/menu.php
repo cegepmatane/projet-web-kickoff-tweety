72779d13da2fecbf681d1d873ef0eb9c57b0ac12
Lucas-GUFFROY <70279313+Lucas-GUFFROY@users.noreply.github.com>
2021-02-10 21:27:07 -0600

    Ajout Page Mission
    Page mission

--- Tweety/menu.php
+++ Tweety/menu.php
@@ -6,6 +6,7 @@
                 <li><a class="menu-item" href="suivis.php">Suivis</a></li>
                 <li><a class="menu-item" href="profil.php">Profil</a></li>
                 <li><a class="menu-item" href="administration.php">Administration</a></li>
+		<li><a class="menu-item" href="pagemission.php">Page Mission</a></li>
                 <li><a class="action" href="">Déconnexion</a></li>
             </ul>
         </nav>
---
+++ Tweety/pagemission.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device, initial-scale=1.0">
+    <link rel="stylesheet" href="decoration/mission.css">>
+
+
+    <title>Page mission</title>
+
+
+
+</head>
+
+<!-- Menu -->
+<?php include('menu.php'); ?>
+
+<body>
+  <p>BIENVENUE SUR LA PAGE MISSION</p>
+
+      <p>Notre mission<br>
+
+Le but de ce site Web est de faire un état des lieux de nos capacités à développer dans un environnement Web et en équipe.<br><br>
+
+Ce site devra contenir : <br>
+<br>
+Une page d'accueil<br>
+Une page décrivant la mission<br>
+Une à deux pages contenant la liste et le détail des données principales<br>
+Une page d'administration permettant d'ajouter, de modifier et de supprimer des données<br>
+Un menu pour la navigation entre les pages<br>
+
+Ce site devra permettre :<br>
+<br>
+L'inscription de membres<br>
+L'authentification de membres<br>
+L'auto-édition de leur profil<br>
+La transaction de paiement en ligne<br>
+L'affichage d'historiques de leur transactions<br>
+L'interactivité Ajax<br>
+La traduction des pages en français et en anglais </p>
+</body>
