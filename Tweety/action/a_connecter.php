0fc6096d3c8646c9df1ce4b899d98d33b9c92338
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-02-25 12:31:58 -0500

    Réinitialisation du cache lors de la connexion #25

--- Tweety/action/a_connecter.php
+++ Tweety/action/a_connecter.php
@@ -5,6 +5,7 @@ $utilisateur = UtilisateurDAO::obtenirUtilisateur($_POST['nomutilisateur']);
 // Vérifier si le mot de passe renseigné
 if ($utilisateur && password_verify($_POST['motdepasse'], $utilisateur->motdepasse)) {
     // Connecter l'utilisateur
+    apcu_clear_cache();
     initialiser_session();
     $_SESSION['utilisateur'] = $utilisateur;
     header('Location: accueil.php');
